22c6d87f36520d8568be8a55d5471ca0adfbd698
calvarez24 <camille.alvarezmartinez@g.austincc.edu>
2025-04-07 02:51:21 +0000

    midterm

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -2,6 +2,9 @@
 from question_b import use_global, global_variable
 
 global_variable = 10
+def use_global():
+    global global_variable
+    global_variable = 20
 
 def main():
     print("Original variable:", global_variable)
